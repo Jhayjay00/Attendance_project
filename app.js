a14a92ec2786526515d7f8376ba31acd193d4e9a
Jhayjay00 <davjiy@gmail.com>
2024-04-02 11:50:21 -0600

    Attendance commit

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express');
+require('dotenv').config;
+const mongoose = require('mongoose');
 const app = express();
 
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
+app.use(express.static('public'));
+
+const url =`mongodb+srv://David:Password@cluster0.4fm7mu2.mongodb.net/`;
+
+
+
+
+mongoose.connect(url)
+.then(() =>{
+    console.log('connected to the MongoDB Databadse');
+})
+.catch((err)=>{
+    console.log(`Error connecting to the database: ${err}`)
+});
 
 
 
